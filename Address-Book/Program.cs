b1d39275f7e43bb097136f78fc10bf7f0fab4d88
Prashant Bhure <Prashantbhure50@gmail.com>
2021-04-24 21:19:44 +0530

    Add file is added

--- Address-Book/Program.cs
+++ Address-Book/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Address_Book
 {
@@ -6,7 +7,30 @@ namespace Address_Book
     {
         static void Main(string[] args)
         {
+            ArrayList list = new ArrayList();
             Console.WriteLine("Welcome To AdressBook");
+            Console.WriteLine("Enter Person FirstName");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Person LastName");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Enter Person AddressDetail");
+            string AddressDetail = Console.ReadLine();
+            Console.WriteLine("Enter Person City");
+            string City = Console.ReadLine();
+            Console.WriteLine("Enter Person State");
+            string State = Console.ReadLine();
+            Console.WriteLine("Enter Person Email");
+            string Email = Console.ReadLine();
+            Console.WriteLine("Enter Person PhoneNumber");
+            int PhoneNo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Person Zip");
+            int Zip = Convert.ToInt32(Console.ReadLine());
+            list.Add(new Address(FirstName, LastName, AddressDetail, City, State, 7000593588, 495223, Email));
+
+            foreach (Address Contact in list)
+            {
+                Console.WriteLine(Contact.FirstName + ", " + Contact.LastName + ", " + Contact.AddressDetail + ", " + Contact.City + ", " + Contact.State + ", " + Contact.PhoneNo + ", " + Contact.Zip + ", " + Contact.Email);
+            }
         }
     }
 }
