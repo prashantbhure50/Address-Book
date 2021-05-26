9ab7b3add79cd191b3b97e63ae3641ef4f3483e7
Prashantbhure50 <prashantbhure50@gmail.com>
2021-05-26 20:30:18 +0530

    [Prashant] ADO.net

---
+++ Address-Book/ContactModle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book
+{
+   public  class ContactModle
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AddressDetail { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string Zip { get; set; }
+        public string Type { get; set; }
+    }
+}
